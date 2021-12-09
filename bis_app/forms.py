81529670255d9b0953a6fa497025f77c3c6762a3
Pedro Echavarria <pechavarria62@gmail.com>
2021-12-09 16:15:58 -0500

    updates

--- bis_app/forms.py
+++ bis_app/forms.py
@@ -3,16 +3,29 @@
 
 # make imports 
 from django import forms
-from .models import post
+from .models import post, Category
+
+# Add Category choices hard coded
+# choices = [('Sports','Sports'),('No Category','No Category'),('Food','Food'),('Veganism','Veganism')]
+
+# Add Category choices but dynamically 
+choices = Category.objects.all().values_list('name','name')
+
+choice_list= []
+
+# Loop trough choices
+for item in choices:
+    choice_list.append(item)
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = post
-        fields = ('title', 'author', 'body', 'created_at')
+        fields = ('title', 'author', 'category', 'body', 'created_at')
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
+            'category': forms.Select(choices=choices, attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
             'created_at': forms.TextInput(attrs={'class': 'form-control'}),
 
